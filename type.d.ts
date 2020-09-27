7ae7408d905dc7b9a393c29857f062be1e4ead3c
trapcodeio <trapcode.io@gmail.com>
2020-09-27 12:48:24 +0100

    Updated Docs

--- type.d.ts
+++ type.d.ts
@@ -191,13 +191,13 @@ declare namespace Xpresser {
          * Get multiple files from post request.
          * @return {Promise<UploadedFiles>}
          */
-        files(field: string, $options?: {
+        files(field: string | string[], $options?: {
             files?: number
             size?: number,
             mimetype?: string | RegExp,
             extensions?: string[],
             includeBody?: boolean,
-            mimetypeForEachField?: { [field: string]: string | RegExp },
+            mimeTypeForEachField?: { [field: string]: string | RegExp },
             extensionsForEachField?: { [field: string]: string[] }
         }): Promise<UploadedFiles>
     }
